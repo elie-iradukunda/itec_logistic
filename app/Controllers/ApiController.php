d8964b906ff7ed1ca9dac06f681136d494b40f34
Nziza Oscar <santosjuniorsoftwareengineer1@gmail.com>
2026-09-19 03:56:17 +0200

    feat: app/Controllers/ApiController.php

---
+++ app/Controllers/ApiController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Controllers;
+
+class ApiController
+{
+    public function health(): void
+    {
+        $this->json(['status' => 'ok', 'app' => \config('app.name'), 'time' => date('c')]);
+    }
+
+    public function me(): void
+    {
+        $this->json([
+            'id' => \current_user_id(),
+            'name' => \current_user_name(),
+            'email' => \current_user_email(),
+            'role' => \current_role(),
+            'role_label' => \role_label(),
+        ]);
+    }
+
+    private function json(array $payload): void
+    {
+        header('Content-Type: application/json; charset=utf-8');
+        echo json_encode($payload);
+    }
+}
